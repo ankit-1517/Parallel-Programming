401e5fbb723543fb45915ed6e386c3332e595415
ankit-1517 <ankit110699@gmail.com>
2021-04-21 14:34:13 +0530

    Updated run/compile files for MPI

--- A2/compile.sh
+++ A2/compile.sh
@@ -1,2 +1,3 @@
 # !/bin/bash
 gcc openmp_strategies.c -o openmp_strategies -fopenmp
+mpicc -g -Wall -o mpi_crout mpi_crout.c
--- A2/run.sh
+++ A2/run.sh
@@ -5,10 +5,11 @@
 # $4 -- strategy
 if [[ $4 -gt 3 ]]
 then
-    echo "MPI"
+    # MPI 
+    time mpiexec -n $3 ./mpi_crout $2 $1
 else
     # OMP
     # ./executable fileName n strategy nThreads
     time ./openmp_strategies $2 $1 $4 $3
-    python3 format_checker.py $2 "output_L_$4_$3.txt" "output_U_$4_$3.txt"
 fi
+python3 format_checker.py $2 "output_L_$4_$3.txt" "output_U_$4_$3.txt"
